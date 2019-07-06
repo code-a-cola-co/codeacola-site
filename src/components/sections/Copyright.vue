70e888f0779495b4ef01071f3668bb28861edb84
code-a-cola <26146122+code-a-cola@users.noreply.github.com>
2019-07-06 01:53:07 -0400

    added breakpoint mixins to control flow for certain items

--- src/components/sections/Copyright.vue
+++ src/components/sections/Copyright.vue
@@ -1,26 +1,49 @@
 <style lang="scss">
   .codeacola-copyright {
     padding: 40px 0;
-    text-align: left;
     background-color: $color-primary;
     color: $color-lightest-gray;
 
-    &_github {
+    &__teaser {
       display: inline-block;
+      text-align: left;
+
+      @include breakpoint-down('md') {
+        display: block;
+        text-align: center;
+        margin-bottom: 6px;
+      }
     }
 
-    &__github {
+    &__links {
       display: inline-block;
       float: right;
-       a {
-         &:link,
-         &:visited,
-         &:active,
-         &:hover {
-           color: $color-lightest-gray;
-           @include font-size-map($h4-font-size, 0.5);
-         }
-       }
+      text-align: right;
+
+      @include breakpoint-down('md') {
+        display: block;
+        float: none;
+        text-align: center;
+      }
+
+      &__twitter,
+      &__github {
+        display: inline-block;
+        @include font-size-map($h6-font-size, 0.5);
+        margin-right: 10px;
+
+        &:link,
+        &:visited,
+        &:active,
+        &:hover {
+          color: $color-lightest-gray;
+          text-decoration: none;
+        }
+
+        &:last-child {
+          margin-right: 0;
+        }
+      }
     }
   }
 </style>
@@ -34,9 +57,20 @@
 
           <span class="codeacola-copyright__teaser">&copy;{{ year }} {{ $static.metaData.siteName }}. Some rights reserved.</span>
 
-          <span class="codeacola-copyright__github">
-            <a href="https://github.com/code-a-cola" title="code-a-cola@github" target="_blank">
-              <i class="codeacola-copyright__github fa fa-github"></i>
+          <span class="codeacola-copyright__links">
+            <a
+              href="https://twitter.com/codeacola"
+              title="code-a-cola@twitter"
+              target="_blank"
+              class="codeacola-copyright__links__twitter">
+              <i class="fa fa-twitter"></i>
+            </a>
+            <a
+              href="https://github.com/code-a-cola"
+              title="code-a-cola@github"
+              target="_blank"
+              class="codeacola-copyright__links__github">
+              <i class="fa fa-github"></i>
             </a>
           </span>
 
